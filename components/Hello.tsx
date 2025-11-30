2de94a78abb7518da6403d0ad972c961a6d15fa6
Joseph <egbenchongjosephayuk@gmail.com>
2025-11-29 21:42:22 -0500

    Completed Next 16 intro course

---
+++ components/Hello.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react'
+
+const Hello = () => {
+    console.log('WHat TYPE of component am I?');
+  return (
+    <div>Hello</div>
+  )
+}
+
+export default Hello
